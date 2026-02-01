9dccf092d02334bf1231517de9fb311c6d05e4b5
rujin <rujinwong@gmail.com>
2026-02-01 18:00:40 +0800

    Add some log points

--- Shopify/Controllers/ShopifyWebhookController.cs
+++ Shopify/Controllers/ShopifyWebhookController.cs
@@ -26,57 +26,70 @@ public class ShopifyWebhookController : ControllerBase
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
+
+        _logger.LogInformation("Order paid webhook received. Order Id {orderId}, order: {@order}", order.Id, order);
         
         var ordersToCreate = new List<Order>();
-        
-        // Get data that we need
-        var deliveryDate = order.NoteAttributes.FirstOrDefault(x => x.Name == "Delivery-Date")?.Value ?? string.Empty;
 
-        var items = order.LineItems.Select(x => new
+        try
         {
-            Line1 = $"{x.Title} x {x.Quantity}",
-            Line2 = string.Join('\n', x.Properties.Where(y => y.Name.IndexOf("Zapiet") == -1).Select(y => $"{y.Name}: {y.Value}")),
-        });
-        
-        var deliveryNote = order.NoteAttributes.FirstOrDefault(x => x.Name == "Delivery-Note")?.Value ?? string.Empty;
-        DateTime.TryParseExact(deliveryDate, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime deliveryDateParsed);
-        
-        var deliveryDateSanitized = deliveryDateParsed.ToString("dd MMMM");
-        var zone = ZoneHelper.ToZone(order.ShippingLines?.FirstOrDefault()?.Code ?? string.Empty);
+            // Get data that we need
+            var deliveryDate = order.NoteAttributes.FirstOrDefault(x => x.Name == "Delivery-Date")?.Value ?? string.Empty;
 
-        var billingContact = (order.BillingAddress?.Phone ?? order.ContactEmail) ?? string.Empty;
-         
-        foreach (var item in items)
-        {
-            var orderToCreate = new Order(
-                order.CreatedAt.ToString("yyyy-MM-ddT:hh:mm.sssZ"), 
-                billingContact,
-                $"{zone}\n{deliveryDateSanitized}",
-                order.OrderNumber.ToString(), 
-                $"{item.Line1}\n{item.Line2}",
-                deliveryNote, 
-                order.ShippingAddress?.FirstName ?? string.Empty,
-                order.ShippingAddress?.Phone ?? string.Empty,
-                $"{order.ShippingAddress?.Address1}\n{order.ShippingAddress?.City}\n{order.ShippingAddress?.Zip}",
-                order.FinancialStatus ?? string.Empty,
-                order.TotalShippingCost.ShopMoney.Amount,
-                order.TotalLineItemPrice.GetValueOrDefault(),
-                order.TotalDiscounts.GetValueOrDefault(),
-                order.TotalPrice.GetValueOrDefault());
+            var items = order.LineItems.Select(x => new
+            {
+                Line1 = $"{x.Title} x {x.Quantity}",
+                Line2 = string.Join('\n', x.Properties.Where(y => y.Name.IndexOf("Zapiet") == -1).Select(y => $"{y.Name}: {y.Value}")),
+            });
             
-            ordersToCreate.Add(orderToCreate);
+            var deliveryNote = order.NoteAttributes.FirstOrDefault(x => x.Name == "Delivery-Note")?.Value ?? string.Empty;
+            DateTime.TryParseExact(deliveryDate, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime deliveryDateParsed);
+            
+            var deliveryDateSanitized = deliveryDateParsed.ToString("dd MMMM");
+            var zone = ZoneHelper.ToZone(order.ShippingLines?.FirstOrDefault()?.Code ?? string.Empty);
+
+            var billingContact = (order.BillingAddress?.Phone ?? order.ContactEmail) ?? string.Empty;
+             
+            foreach (var item in items)
+            {
+                var orderToCreate = new Order(
+                    order.CreatedAt.ToString("yyyy-MM-ddT:hh:mm.sssZ"), 
+                    billingContact,
+                    $"{zone}\n{deliveryDateSanitized}",
+                    order.OrderNumber.ToString(), 
+                    $"{item.Line1}\n{item.Line2}",
+                    deliveryNote, 
+                    order.ShippingAddress?.FirstName ?? string.Empty,
+                    order.ShippingAddress?.Phone ?? string.Empty,
+                    $"{order.ShippingAddress?.Address1}\n{order.ShippingAddress?.City}\n{order.ShippingAddress?.Zip}",
+                    order.FinancialStatus ?? string.Empty,
+                    order.TotalShippingCost.ShopMoney.Amount,
+                    order.TotalLineItemPrice.GetValueOrDefault(),
+                    order.TotalDiscounts.GetValueOrDefault(),
+                    order.TotalPrice.GetValueOrDefault());
+                
+                ordersToCreate.Add(orderToCreate);
+            }
+            
+            var rows = ordersToCreate.Select(x => GoogleSheetsHelper.ToRow(x)).ToList();
+            var body = new ValueRange
+            {
+                Values = rows
+            };
+            
+            _logger.LogInformation("Writing order {orderId} to google sheets, number of rows: {count}", order.Id, rows.Count);
+
+            var request = _sheets.Spreadsheets.Values.Append(body, SpreadsheetId, "Sheet1!A1");
+            request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+
+            await request.ExecuteAsync();
+            
+            return Ok();
         }
-        
-        var rows = ordersToCreate.Select(x => GoogleSheetsHelper.ToRow(x)).ToList();
-        var body = new ValueRange
+        catch (Exception ex)
         {
-            Values = rows
-        };
-
-        var request = _sheets.Spreadsheets.Values.Append(body, SpreadsheetId, "Sheet1!A1");
-        request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
-
-        await request.ExecuteAsync();
-        return Ok();
+            _logger.LogError(ex, "Error occured when processing order {orderId}", order.Id);
+            return BadRequest($"Could not process order {order.Id}");
+        }
     }
 }
